0945f6bdc4515b00a6bfb42b52db500a1f8be3a3
yeonseong <yeonseong.dev@gmail.com>
2023-05-09 22:02:06 +0900

    FEAT: colorful wave

--- wave.js
+++ wave.js
@@ -1,12 +1,11 @@
 import { Point } from "./point.js";
 
 export class Wave {
-  constructor() {
-    this.totalPoints = 6;
-    this.totalSegments = this.totalPoints - 1;
-    this.color = "#ff0000";
+  constructor(index, totalPoints, color) {
+    this.index = index;
+    this.totalPoints = totalPoints;
+    this.color = color;
     this.points = [];
-    this.segmentWidth = 0;
   }
 
   resize(stageWidth, stageHeight) {
@@ -16,20 +15,44 @@ export class Wave {
     this.centerX = stageWidth / 2;
     this.centerY = stageHeight / 2;
 
+    this.pointGap = this.stageWidth / (this.totalPoints - 1);
+
     this.init();
   }
 
   init() {
-    this.point = new Point(this.centerX, this.centerY);
+    this.points = [];
+
+    for (let i = 0; i < this.totalPoints; i++) {
+      const point = new Point(this.index + i, this.pointGap * i, this.centerY);
+      this.points[i] = point;
+    }
   }
 
   draw(ctx) {
     ctx.beginPath();
-    ctx.fillStyle = "#ff0000";
+    ctx.fillStyle = this.color;
 
-    this.point.update();
+    let prevX = this.points[0].x;
+    let prevY = this.points[0].y;
 
-    ctx.arc(this.point.x, this.point.y, 30, 0, 2 * Math.PI);
+    ctx.moveTo(prevX, prevY);
+
+    for (let i = 1; i < this.totalPoints; i++) {
+      if (i < this.totalPoints - 1) {
+        this.points[i].update();
+      }
+
+      const cx = (prevX + this.points[i].x) / 2;
+      const cy = (prevY + this.points[i].y) / 2;
+
+      ctx.quadraticCurveTo(prevX, prevY, cx, cy);
+      prevX = this.points[i].x;
+      prevY = this.points[i].y;
+    }
+
+    ctx.lineTo(prevX, prevY);
     ctx.fill();
+    ctx.closePath();
   }
 }
